74f57ee795eb9e7dd41f212efaf8bc3682ed6a5b
ryankert01 <91534261+ryankert01@users.noreply.github.com>
2021-10-26 17:03:19 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int f (int n) {
+	if (n <= 100) {
+		n = f (f (n + 11));
+	}
+	else {
+		n = n - 10;
+	}
+	return n;
+}
+int main() 
+{
+	int input;
+	while (cin >> input && input) {
+		cout << "f91(" << input << ") = " << f(input) << endl;
+	}
+}
