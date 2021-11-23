f6ec920950bc01f80cae8f340ace387fc760562f
Ryan <91534261+ryankert01@users.noreply.github.com>
2021-11-24 03:13:37 +0800

    Update UVA-10550.cpp

--- UVA-10550.cpp
+++ UVA-10550.cpp
@@ -7,36 +7,11 @@ int main(){
     int a, b, c, d;
     while(cin >> a >> b >> c >> d && !((a == 0 && b == 0) && (c == 0 && d == 0)))
     {
-        int degree = 1080;
-        int change = 0;
-        int at = a;
-        if (at < b)
-        {
-            change += 40 - abs(b - at);
-        }
-        else
-        {
-            change += abs(b - at);
-        }
-        at = b;
-        if (at > c)
-        {
-            change += 40 - abs(c - at);
-        }
-        else
-        {
-            change += abs(c - at);
-        }
-        at = c;
-        if (at < d)
-        {
-            change += 40 - abs(d - at);
-        }
-        else
-        {
-            change += abs(d - at);
-        }
-        at = d;
-        cout << degree + change * 9 << endl;
+        int degree = 120;
+        degree += (40 + a - b) % 40;
+        degree += (40 - b + c) % 40;
+        degree += (40 + c - d) % 40;
+        cout << degree * 9 << endl;
+        
     }
 }
