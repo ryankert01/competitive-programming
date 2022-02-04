8468c777b13e51727a36f2cf105710f59712966b
Ryan <91534261+ryankert01@users.noreply.github.com>
2022-02-04 19:06:20 +0800

    ac

--- UVA-10783.cpp
+++ UVA-10783.cpp
@@ -6,8 +6,7 @@ int main(){
     cin >> t;
     for(int i = 1; i <= t; i++){
         int a,b;
-        cin >> a;
-        cin >> b;
+        cin >> a >> b;
         if(!(a%2))a++;
         if(!(b%2))b--;
         cout << "Case " << i << ": "<< (a+b)*((b-a)/2+1)/2 << endl;
