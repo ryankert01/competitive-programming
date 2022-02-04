c39a1a0089363aaf87eb779e1291c62e1f4115d0
ryankert01 <ryan980053@gmail.com>
2022-02-04 18:17:28 +0800

    ac

---
+++ UVA-10783.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    for(int i = 1; i <= t; i++){
+        int a,b;
+        cin >> a;
+        cin >> b;
+        if(!(a%2))a++;
+        if(!(b%2))b--;
+        if(a==b){
+            cout << "Case " << i << ": "<< a << endl;
+            continue;
+        } 
+        cout << "Case " << i << ": "<< (a+b)*((b-a)/2+1)/2 << endl;
+    }
+}
