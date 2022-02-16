fc8d843a5f4b3a3ab945a5597185640f6069e407
Ryan <91534261+ryankert01@users.noreply.github.com>
2022-02-16 09:56:41 +0800

    aa

---
+++ 10235.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+//return true if and only if the number is a primary number.
+bool isPrimeNum(int num){
+    for(int i = 2; i < num; i++){
+        if(num%i == 0) return false;
+    }
+    return true;
+}
+
+//return the reversed digit
+int reverseDigit(int num){
+    int ans = 0, flag = 0;
+    while(num){
+        if(flag) ans *= 10;
+        ans += num%10;
+        num /= 10;
+        flag = 1;
+    }
+    return ans;
+}
+
+
+
+
+int main(){
+    int numb;
+    while(cin >> numb){
+        cout << numb;
+        if(isPrimeNum(numb)){
+            numb = reverseDigit(numb);
+            if(isPrimeNum(numb)) cout << " is emirp.\n";
+            else cout << " is prime.\n";
+        }
+        else cout << " is not prime.\n";
+    }
+
+}
