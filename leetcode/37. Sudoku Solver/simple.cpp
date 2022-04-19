13d9e573bedd5c7bd8ccfd6ff2beab6493e32ec4
Ryan <91534261+ryankert01@users.noreply.github.com>
2022-04-19 17:45:46 +0800

    Create simple.cpp

---
+++ leetcode/37. Sudoku Solver/simple.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    
+    // return true if and only if this move obay the law of Sudoku
+    bool obayRule(int row, int col, char c, vector< vector < char > >& board){
+        for(int i = 0; i < 9; i++)
+        {
+            if(board[i][col] == c)
+                return false;
+            if(board[row][i] == c)
+                return false;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3] == c)
+                return false;
+        }
+        return true;
+    }
+    
+    bool dfs(vector<vector<char>>& board){
+        for(int i = 0; i < board.size(); i++)
+        {
+            for(int j = 0; j < board[0].size(); j++)
+            {
+                if(board[i][j] == '.')
+                {
+                    for(char pos = '1'; pos <= '9'; pos++)
+                    {
+                        if(obayRule(i,j,pos,board))
+                        {
+                            board[i][j] = pos;
+                            if(dfs(board) == true)
+                                return true;
+                            else
+                            {
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    void solveSudoku(vector<vector<char>>& board) {
+        dfs(board);
+    }
+};
