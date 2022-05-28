098b3e8a9fedf6f80b2a954eabab4e83ab677837
Ryan <91534261+ryankert01@users.noreply.github.com>
2022-05-29 00:11:45 +0800

    Update map.cpp

--- leetcode/2284. Sender with Largest Word Count/map.cpp
+++ leetcode/2284. Sender with Largest Word Count/map.cpp
@@ -1,10 +1,31 @@
 class Solution {
 public:
-    bool digitCount(string num) {
-        string nums(num.size(),'0');
-        for(char& i : num)
-            nums[i-'0']++;
-        if(num == nums)return true;
-        else return false;
-    }
+    string largestWordCount(vector<string>& messages, vector<string>& senders) {
+        unordered_map<string, int>mp;
+        int s = messages.size();
+        int m = INT_MIN,cs;
+        string ms;
+        for(int i = 0; i < s; i++)
+        {
+            cs = 1;
+            for(char& j : messages[i])
+                if(j == ' ')cs++;
+            mp[senders[i]] += cs;
+        }
+        for(auto i : mp)
+        {
+            if(i.second > m)
+            {
+                m = i.second;
+                ms = i.first;
+            }
+            else if(i.second == m && ms.compare(i.first) < 0)
+            {
+                m = i.second;
+                ms = i.first;
+            }
+        }
+        return ms;
+    }// lexicographically largest name.!!!!!!!!!!!!!!!
+
 };
