bfd3452da97e702c966d9c5e583febfb33ee1af8
Ryan <91534261+ryankert01@users.noreply.github.com>
2022-06-03 14:15:16 +0800

    Create 2d_dp.cpp

---
+++ leetcode/304. Range Sum Query 2D - Immutable/2d_dp.cpp
@@ -0,0 +1,35 @@
+// row-oriented approach, dynamic programming
+class NumMatrix {
+public:
+    vector<vector<int>>v;
+    NumMatrix(vector<vector<int>>& matrix) :v(matrix){
+        for(int i = 0; i < v.size(); i++)
+            for(int j = 0; j < v[0].size();j++)
+            {
+                if(i && j)
+                    v[i][j] += v[i-1][j] + v[i][j-1] - v[i-1][j-1];
+                else if(j)
+                    v[i][j] += v[i][j-1];
+                else if(i)
+                    v[i][j] += v[i-1][j];
+            }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) 
+    {
+        int ans = v[row2][col2];
+        if(row1)
+            ans -= v[row1-1][col2];
+        if(col1)
+            ans -= v[row2][col1-1];
+        if(row1 && col1)
+            ans += v[row1-1][col1-1];
+        return ans;
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
