ecf3f5bd00105ed7be76e5f9efd4af81d52404a3
Ryan <91534261+ryankert01@users.noreply.github.com>
2022-06-12 00:30:39 +0800

    Create lower_bound.cpp

---
+++ leetcode/2300. Successful Pairs of Spells and Potions/lower_bound.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        sort(potions.begin(), potions.end());
+        vector<int>res(spells.size(),0);
+        int ss = spells.size();
+        int ps = potions.size();
+        long long mi;
+        for(int i = 0; i < ss; i++)
+        {
+            mi = (success + spells[i] - 1 ) / spells[i];
+            int a =  lower_bound(potions.begin(), potions.end(), mi)-potions.begin();
+            res[i] = ps-a;
+        }
+        return res;
+    }
+};
